85b98d696089a61b13c93fcd8afd2108d9e81ace
César Patiño <cpatino@copaair.com>
2022-04-03 18:05:55 -0500

    Typography complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,106 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
 <main>
-	<div class="container bg-bg-light-blue">
-    <h1>Hello {name}!</h1>
+	<div class="container bg-bg-light-blue py-96">
+    <h3 class="text-primary-main mb-24">Regular Title Hierarchy styles (H1 - H4)</h3>
+    <div class="flex justify-between items-baseline">
+    <h1>Heading 1</h1>
     <h2>Heading 2</h2>
     <h3>Heading 3</h3>
     <h4>Heading 4</h4>
-    <div class="cm-display--big">Display Big</div>
-    <div class="cm-display--normal">Display Normal</div>
-    <div class="cm-display--small">Display Small</div>
-    <div class="cm-display--tiny">Display Tiny</div>
-    <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+    </div>
+    <h3 class="text-primary-main mt-64 mb-24">Display Text styles</h3>
+    <div class="flex justify-between items-baseline">
+      <div class="cm-display--big">Display Big</div>
+      <div class="cm-display--normal">Display Normal</div>
+      <div class="cm-display--small">Display Small</div>
+      <div class="cm-display--tiny">Display Tiny</div>
+    </div>
+    <h3 class="text-primary-main mt-64 mb-24">Body Text variants</h3>
+    <div class="flex justify-between items-baseline">
+      <p class="text-20-32">Caption Large</p>
+      <p class="text-12-16">Caption</p>
+      <p>Body 1</p>
+      <p class="text-14-20">Body 2</p>
+      <p class="cm-overline">Overline</p>
+    </div>
+    <h3 class="text-primary-main mt-64 mb-24">Body Text style overrides</h3>
+    <div class="flex justify-between items-baseline p-8">
+      <p>Body Large / Primary</p>
+      <p class="text-14-20">Body Normal / Primary</p>
+      <p class="text-12-16">Body small / Primary</p>
+    </div>
+    <div class="flex justify-between items-baseline p-8">
+      <em>Body Large / Emphasis</em>
+      <em class="text-14-20">Body Normal / Emphasis</em>
+      <em class="text-12-16">Body small / Emphasis</em>
+    </div>
+    <div class="flex justify-between items-baseline p-8">
+      <a href="#">Body Large / Link</a>
+      <a href="#" class="text-14-20">Body Normal / Link</a>
+      <a href="#" class="text-12-16">Body small / Link</a>
+    </div>
+    <div class="flex justify-between items-baseline p-8">
+      <a href="#" class="text-grey-600 underline">Body Large / Link Secondary</a>
+      <a href="#" class="text-14-20 text-grey-600 underline">Body Normal / Link Secondary</a>
+      <a href="#" class="text-12-16 text-grey-600 underline">Body small / Link Secondary</a>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8 rounded-t-lg">
+      <p class="text-grey-200">Body Large / Primary Invert</p>
+      <p class="text-14-20 text-grey-200">Body Normal / Primary Invert</p>
+      <p class="text-12-16 text-grey-200">Body small / Primary Invert</p>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8">
+      <em class="text-grey-50">Body Large / Emphasis Invert</em>
+      <em class="text-14-20 text-grey-50">Body Normal / Emphasis Invert</em>
+      <em class="text-12-16 text-grey-50">Body small / Emphasis Invert</em>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8">
+      <a href="#" class="text-grey-200 underline">Body Large / Link Invert</a>
+      <a href="#" class="text-14-20 text-grey-200 underline">Body Normal / Link Invert</a>
+      <a href="#" class="text-12-16 text-grey-200 underline">Body small / Link Invert</a>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8 rounded-b-lg">
+      <a href="#" class="text-grey-200 underline">Body Large / Link Secondary Invert</a>
+      <a href="#" class="text-14-20 text-grey-200 underline">Body Normal / Link Secondary Invert</a>
+      <a href="#" class="text-12-16 text-grey-200 underline">Body small / Link Secondary Invert</a>
+    </div>
+    <div class="flex justify-between items-baseline mt-24 p-8">
+      <p class="text-20-32 text-grey-700">Caption Large / Primary</p>
+      <p class="text-grey-700">Caption Normal / Primary</p>
+      <p class="text-14-20 text-grey-700">Caption small / Primary</p>
+      <p class="text-12-16 text-grey-700">Caption tiny / Primary</p>
+    </div>
+    <div class="flex justify-between items-baseline p-8">
+      <p class="text-20-32 text-grey-600">Caption Large / Secondary</p>
+      <p class="text-grey-600">Caption Normal / Secondary</p>
+      <p class="text-14-20 text-grey-600">Caption small / Secondary</p>
+      <p class="text-12-16 text-grey-600">Caption tiny / Secondary</p>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8 rounded-t-lg">
+      <p class="text-20-32 text-grey-50">Caption Large / Primary Invert</p>
+      <p class="text-grey-50">Caption Normal / Primary Invert</p>
+      <p class="text-14-20 text-grey-50">Caption small / Primary Invert</p>
+      <p class="text-12-16 text-grey-50">Caption tiny / Primary Invert</p>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8 rounded-b-lg">
+      <p class="text-20-32 text-grey-200">Caption Large / Secondary Invert</p>
+      <p class="text-grey-200">Caption Normal / Secondary Invert</p>
+      <p class="text-14-20 text-grey-200">Caption small / Secondary Invert</p>
+      <p class="text-12-16 text-grey-200">Caption tiny / Secondary Invert</p>
+    </div>
+    <div class="flex justify-between items-baseline mt-24 p-8">
+      <p class="cm-overline">Overline Normal / Primary</p>
+      <p class="text-12-16">Overline small / Primary</p>
+    </div>
+    <div class="flex justify-between items-baseline p-8">
+      <em class="cm-overline">Overline Normal / Emphasis</em>
+      <em class="text-12-16">Overline small / Emphasis</em>
+    </div>
+    <div class="flex justify-between items-baseline bg-primary-main p-8 rounded-md">
+      <p class="cm-overline text-grey-50">Overline Normal / Primary Invert</p>
+      <p class="text-12-16 text-grey-50">Overline small / Primary Invert</p>
+    </div>
   </div>
 </main>
