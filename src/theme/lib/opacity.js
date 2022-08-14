2ad0807757f8c8b13526d56bb9b0a0f4d6f1583c
Cesar Patino <cpatino@copaair.com>
2022-08-14 12:55:18 -0500

    fixing opacity

--- src/theme/lib/opacity.js
+++ src/theme/lib/opacity.js
@@ -1,9 +1,7 @@
 const OPACITY_EXTENSION = {
-  faded: {
-    primary: '0.4',
-    secondary: '0.5',
-    status: '0.15'
-  }
+  'primary-faded': '0.4',
+  'secondary-faded': '0.5',
+  'status-faded': '0.15'
 }
 
 const getOverrides = () => {
